cae0e8ace22f4a858699faf99993557af72a6c63
bannmoore <moore.brittanyann@gmail.com>
2018-09-20 14:17:13 -0400

    feat: Upgrade postcss-loader.
    Side Effects:
    - Replace postcss-cssnext (deprecated) with postcss-preset-env.

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,5 +1,10 @@
+/*
+ * Configuration passed to the postcss-loader in Webpack.
+ * Docs: https://github.com/postcss/postcss-loader#configuration
+ */
+
 module.exports = {
   plugins: {
-    'postcss-cssnext': {}
+    'postcss-preset-env': {}
   }
 }
