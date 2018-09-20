0057a37d6b3e0f197beac0d0f41306dce391c716
bannmoore <moore.brittanyann@gmail.com>
2018-09-20 14:06:53 -0400

    refactor: Clean up webpack configs and add comments.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,33 +4,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  mode: process.env.MODE,
+  entry: './src/index.js',
 
   output: {
-    publicPath: ''
+    // publicPath is prefixed onto every URL created by runtimes or loaders.
+    // Docs: https://webpack.js.org/configuration/output/#output-publicpath
+    publicPath: '/'
   },
 
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.(eot|svg)$/,
+        use: 'file-loader?name=assets/[name].[hash:20].[ext]'
+      },
+      {
+        test: /\.(jpg|png|gif|otf|ttf|woff|woff2|cur|ani)$/,
+        use: 'url-loader?name=assets/[name].[hash:20].[ext]&limit=10000'
+      },
+      // CSS imports from node_modules are extracted into an external stylesheet.
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: [{ loader: 'css-loader', options: { minimize: true } }]
+        }),
+        include: [/node_modules/]
+      }
     ]
   },
 
   plugins: [
+    // HtmlWebpackPlugin automatically generates an index.html file based on the provided template.
     new HtmlWebpackPlugin({
       template: './config/index.template.html'
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.MODE)
-    }),
+    // ExtractTextPlugin allows us to extract certain styles to be loaded separately from the app.
     new ExtractTextPlugin('styles/[name].css')
   ],
 
   resolve: {
-    extensions: ['.js', '.jsx'],
+    // Allows certain file extensions to be imported without specifying the extension.
+    // For example, `import Component from 'component'` rather than `import Component from
+    // 'component.js'`.
+    extensions: ['.js', '.jsx', '.json'],
+
+    // Allows packages in node_modules to be loaded by name.
     modules: [path.resolve(__dirname, './../node_modules')]
   },
 
+  // Specifies what data is displayed when running the `webpack` command.
   stats: {
     all: false,
     assets: true,
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,8 @@
-const mode = process.env.MODE || 'development'
-module.exports = require(`./config/webpack.${mode}.js`)
+/*
+ * Webpack looks for a webpack.config.js file in the root directory.
+ * Using the environment variable MODE, this file loads the correct configuration
+ * from the `config` directory.
+ */
+
+const mode = process.env.MODE || 'production';
+module.exports = require(`./config/webpack.${mode}.js`);
